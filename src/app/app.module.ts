2e987d87683652475c3eb74e32646e790c868cd5
alexandreg67 <alexandre.ag.67@gmail.com>
2023-07-12 12:04:03 +0200

    récuperation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { PageHomeComponent } from './pages/page-home/page-home.component';
 import { PageMyPlantsComponent } from './pages/page-my-plants/page-my-plants.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { PageAdminComponent } from './pages/page-admin/page-admin.component';
+import { PlantListComponent } from './components/plant-list/plant-list.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { PageAdminComponent } from './pages/page-admin/page-admin.component';
     PageHomeComponent,
     PageMyPlantsComponent,
     PageNotFoundComponent,
-    PageAdminComponent
+    PageAdminComponent,
+    PlantListComponent
   ],
   imports: [
     BrowserModule,
